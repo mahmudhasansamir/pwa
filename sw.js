25841ea109c89cc0407df4b8c4933f2874e69718
mahmudhasansamir <140411458+mahmudhasansamir@users.noreply.github.com>
2024-12-27 23:00:16 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,14 @@
-const CACHE_NAME = 'hasan-tech-cache-v1'; // ক্যাশ নাম
-const urlsToCache = [
-  '/', // হোমপেজ
-  '/index.html', // আপনার গিটহাবে আপলোড করা index.html ফাইল
-  '/manifest.json', // মেনিফেস্ট ফাইল
-  '/icon512_maskable.png', // অ্যাপ আইকন
-  '/icon512_rounded.png', // আরেকটি অ্যাপ আইকন
-  'https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap', // গুগল ফন্ট
-];
-
-// ইনস্টল ইভেন্ট: ক্যাশে করার জন্য প্রাথমিক ফাইল যোগ করা
+// Empty Service Worker (Just for enabling PWA installation)
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  console.log('Service Worker Installed');
+  self.skipWaiting(); // Activate immediately
 });
 
-// অ্যাক্টিভেট ইভেন্ট: পুরানো ক্যাশ মুছে ফেলা
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+  console.log('Service Worker Activated');
 });
 
-// ফেচ ইভেন্ট: ক্যাশ থেকে ডাটা সরবরাহ করা
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // ক্যাশ থেকে রেসপন্স ফেরত দেওয়া বা নেটওয়ার্ক থেকে নতুন ফেচ করা
-      return response || fetch(event.request);
-    })
-  );
+  // No offline support
+  console.log('Fetch event for', event.request.url);
 });
